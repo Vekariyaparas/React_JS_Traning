e98290499096959f3e945ce1d89c4ff0e6992554
paras vekariya <113162042+Vekariyaparas@users.noreply.github.com>
2023-11-08 16:34:40 +0530

    Update Form.jsx

--- 1) React Basics/basic-component/src/Components/Form.jsx
+++ 1) React Basics/basic-component/src/Components/Form.jsx
@@ -7,21 +7,21 @@ export default function Form() {
                 <form className='w-50 mx-auto mt-5 border p-4'>
                     <fieldset className=''>
                         <legend className='mb-3 text-center'>Contact Form</legend>
-                        <div class="input-group mb-3">
-                            <span class="input-group-text p-2" id="basic-addon1"><i className='bi bi-person'></i></span>
-                            <input type="text" class="form-control" placeholder="First Name" aria-label="Username" aria-describedby="basic-addon1" />
+                        <div className="input-group mb-3">
+                            <span className="input-group-text p-2" id="basic-addon1"><i className='bi bi-person'></i></span>
+                            <input type="text" className="form-control" placeholder="First Name" aria-label="Username" aria-describedby="basic-addon1" />
                         </div>
-                        <div class="input-group mb-3">
-                            <span class="input-group-text p-2" id="basic-addon1"><i className='bi bi-person'></i></span>
-                            <input type="text" class="form-control" placeholder="Last Name" aria-label="Username" aria-describedby="basic-addon1" />
+                        <div className="input-group mb-3">
+                            <span className="input-group-text p-2" id="basic-addon1"><i className='bi bi-person'></i></span>
+                            <input type="text" className="form-control" placeholder="Last Name" aria-label="Username" aria-describedby="basic-addon1" />
                         </div>
-                        <div class="input-group mb-3">
-                            <span class="input-group-text p-2" id="basic-addon1"><i className='bi bi-envelope'></i></span>
-                            <input type="text" class="form-control" placeholder="Email" aria-label="Username" aria-describedby="basic-addon1" />
+                        <div className="input-group mb-3">
+                            <span className="input-group-text p-2" id="basic-addon1"><i className='bi bi-envelope'></i></span>
+                            <input type="text" className="form-control" placeholder="Email" aria-label="Username" aria-describedby="basic-addon1" />
                         </div>
-                        <div class="input-group mb-2">
-                            <span class="input-group-text p-2" id="basic-addon1"><i className='bi bi-telephone'></i></span>
-                            <input type="text" class="form-control" placeholder="Contact Number" aria-label="Username" aria-describedby="basic-addon1" />
+                        <div className="input-group mb-2">
+                            <span className="input-group-text p-2" id="basic-addon1"><i className='bi bi-telephone'></i></span>
+                            <input type="text" className="form-control" placeholder="Contact Number" aria-label="Username" aria-describedby="basic-addon1" />
                         </div>
                     </fieldset>
                 </form>
