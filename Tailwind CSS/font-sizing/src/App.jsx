5ab1043476095af2be95459c6265bc2d3bb81c09
Paras Vekariya <113162042+Vekariyaparas@users.noreply.github.com>
2023-12-08 19:02:59 +0530

    Add files via upload

---
+++ Tailwind CSS/font-sizing/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <p className='font-sans text-xs text-slate-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore omnis placeat aspernatur laborum blanditiis, sit natus vero labore rem repellat cumque soluta dolores, fugit aliquam, atque maxime architecto. Deleniti, sit.</p>
+      <p className='font-serif text-sm text-zinc-900'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore omnis placeat aspernatur laborum blanditiis, sit natus vero labore rem repellat cumque soluta dolores, fugit aliquam, atque maxime architecto. Deleniti, sit.</p>
+      <p className='font-sans text-base text-red-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore omnis placeat aspernatur laborum blanditiis, sit natus vero labore rem repellat cumque soluta dolores, fugit aliquam, atque maxime architecto. Deleniti, sit.</p>
+      <p className='font-semibold text-lg text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore omnis placeat aspernatur laborum blanditiis, sit natus vero labore rem repellat cumque soluta dolores, fugit aliquam, atque maxime architecto. Deleniti, sit.</p>
+      <p className='font-medium text-xl text-justify text-yellow-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore omnis placeat aspernatur laborum blanditiis, sit natus vero labore rem repellat cumque soluta dolores, fugit aliquam, atque maxime architecto. Deleniti, sit.</p>
+      <p className='font-thin text-2xl tracking-widest text-neutral-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore omnis placeat aspernatur laborum blanditiis, sit natus vero labore rem repellat cumque soluta dolores, fugit aliquam, atque maxime architecto. Deleniti, sit.</p>
+      <p className='font-light tracking-tight text-cyan-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore omnis placeat aspernatur laborum blanditiis, sit natus vero labore rem repellat cumque soluta dolores, fugit aliquam, atque maxime architecto. Deleniti, sit.</p>
+      <p className='italic tracking-tighter text-sky-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore omnis placeat aspernatur laborum blanditiis, sit natus vero labore rem repellat cumque soluta dolores, fugit aliquam, atque maxime architecto. Deleniti, sit.</p>
+      <p className='uppercase'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore omnis placeat aspernatur laborum blanditiis, sit natus vero labore rem repellat cumque soluta dolores, fugit aliquam, atque maxime architecto. Deleniti, sit.</p>
+      <p className='capitalize'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore omnis placeat aspernatur laborum blanditiis, sit natus vero labore rem repellat cumque soluta dolores, fugit aliquam, atque maxime architecto. Deleniti, sit.</p>
+      <p className='indent-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore omnis placeat aspernatur laborum blanditiis, sit natus vero labore rem repellat cumque soluta dolores, fugit aliquam, atque maxime architecto. Deleniti, sit.</p>
+    </div>
+  )
+}
+
+export default App
