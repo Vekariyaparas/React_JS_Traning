33b67872d8757120ba6d70b1245e6b3bcbdfb55b
Paras Vekariya <113162042+Vekariyaparas@users.noreply.github.com>
2023-12-11 11:29:08 +0530

    Add files via upload

---
+++ 4) Routing in React/intro-react-router/src/components/Blog.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Blog = () => {
+  return (
+    <div>
+      <h1 className="p-8 text-lg">
+        This is <span className="text-sky-700 font-bold">Blog</span> page
+      </h1>
+    </div>
+  );
+};
+
+export default Blog;
