f614330a57b2078ddce327116aaf4c2196f7fec0
Paras Vekariya <113162042+Vekariyaparas@users.noreply.github.com>
2023-12-25 17:30:58 +0530

    Add files via upload

---
+++ Matireal-UI/material-ui-setup/src/App.js
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <Typography variant='h3'>Hy, I am using Matireal UI</Typography>
+    </div>
+  )
+}
+
+export default App
