6599cc59273cfcdfb92cbb1cc84eae6eca460cfd
Paras Vekariya <113162042+Vekariyaparas@users.noreply.github.com>
2023-12-28 11:24:25 +0530

    Add files via upload

---
+++ 6) Advanced React Patterns & Techniques/higher-order-components/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { MyComponentWithLoading } from "./components/Mycoponents";
+
+const App = () => {
+  return (
+    <>
+      <h1>React</h1>
+      <h1>My App</h1>
+      <MyComponentWithLoading />
+    </>
+  );
+};
+
+export default App;
